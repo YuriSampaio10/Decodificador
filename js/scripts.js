e40187fa7fbcc5c5ce5d4b7002051b1e6fbc56c3
Yuri Sampaio <sampaio.yuri31@gmail.com>
2022-04-30 19:54:42 -0300

    capturando o texto digitado pelo usuário

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+//faz a foto que está dentro do retangulo sair do display
 function myFunction() {
   document.getElementById("foto").style.display = "none";
+
+  }
+
+  var capturaTexto = document.querySelector("#criptografar"); // obtem o botão do html
+  capturaTexto.addEventListener("click", function(){
+  	var textoDigitado = obtemTextoDigitado();
+  	alert(textoDigitado);
+
+ 
+	
+
+
+  })
+
+  function obtemTextoDigitado(){
+  	caixaTexto = document.getElementById("caixa-teste").value;
+  	return caixaTexto
   }
