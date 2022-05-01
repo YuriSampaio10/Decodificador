2533cf8763176787f215468f787b21065784ddf2
Yuri Sampaio <sampaio.yuri31@gmail.com>
2022-04-30 22:10:12 -0300

    resetando o texto digitado pelo usuário

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
-//faz a foto que está dentro do retangulo sair do display
+//faz a foto que está dentro do retangulo e o texto sair do display
 function myFunction() {
   document.getElementById("foto").style.display = "none";
-
   }
 
+//criptografar
   var capturaTexto = document.querySelector("#criptografar"); // obtem o botão do html
-  capturaTexto.addEventListener("click", function(){
+  capturaTexto.addEventListener("click", function(){ // escuta o click e aplica a função
   	var textoDigitado = obtemTextoDigitado();
-  	alert(textoDigitado);
-
- 
-	
+  	console.log(textoDigitado);
+  	var reseta = resetaTexto();
+  	
 
 
   })
 
   function obtemTextoDigitado(){
   	caixaTexto = document.getElementById("caixa-teste").value;
-  	return caixaTexto
+  	return caixaTexto;
+  }
+
+  function resetaTexto(){
+  	var reseta = document.getElementById("caixa-teste").value = "";
+  	return reseta;
   }
