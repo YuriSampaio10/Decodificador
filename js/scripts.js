9d42e979590a16f0b66f474a356066b6345b1ef8
Yuri Sampaio <sampaio.yuri31@gmail.com>
2022-04-30 23:09:05 -0300

    criando o parágrafo e o adicionanco como filho do retangulo

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,15 @@ function myFunction() {
   var capturaTexto = document.querySelector("#criptografar"); // obtem o botão do html
   capturaTexto.addEventListener("click", function(){ // escuta o click e aplica a função
   	var textoDigitado = obtemTextoDigitado();
-  	console.log(textoDigitado);
   	var reseta = resetaTexto();
+  	var paragrafo = criaParagrafo();
+  	var adiciona = adicionaAoRetangulo(paragrafo);
   	
 
+  	
+console.log(textoDigitado);
+console.log(paragrafo);
+console.log(adiciona);
 
   })
 
@@ -20,6 +25,17 @@ function myFunction() {
   }
 
   function resetaTexto(){
-  	var reseta = document.getElementById("caixa-teste").value = "";
+  	var reseta = document.getElementById("caixa-teste").value = ""; 
   	return reseta;
+  }
+
+  function criaParagrafo(){
+  	var textoP = document.createElement("p"); 
+  	return textoP
+  }
+
+  function adicionaAoRetangulo(paragrafo){
+  	var adicionaParagrafo = document.querySelector("#retangulo");
+  	adicionaParagrafo.appendChild(paragrafo);
+  	return adicionaParagrafo;
   }
