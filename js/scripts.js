1d431059c8094404fb187200ca108d6c9584cfcc
Yuri Sampaio <sampaio.yuri31@gmail.com>
2022-05-02 16:39:24 -0300

    adicionando novos validadores

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 var input = document.querySelector("#textoOutput");
-caixaTexto = document.getElementById("caixa-teste").value;
+caixaTexto = document.getElementById("caixa-teste");
+
+//inicia com o cursor no campo de digitação
+caixaTexto.focus();
+
+//esta regex aceita somente letras minúsculas e sem acento, inclusive escritos em múltiplas linhas.
+var regex = /^[a-z\s]+$/;
 
 //faz a foto que está dentro do retangulo e o texto sair do display
 function myFunction() {
-	caixaTexto = document.getElementById("caixa-teste").value;
-	if (caixaTexto) {
-		document.getElementById("foto").style.display = "none";
+  caixaTexto = document.getElementById("caixa-teste").value;
+  if (caixaTexto) {
+    document.getElementById("foto").style.display = "none";
 }};
-
-
 
 //criptografar
 var encriptar = document.querySelector("#criptografar"); // obtem o botão do html
 encriptar.addEventListener("click", function(){ // escuta o click e aplica a função
-  	var encriptado = obtemTextoDigitadoECriptografa();
-  	var adiciona = adicionaAoRetangulo(input, encriptado);
-  	var reseta = resetaTexto();
+    var encriptado = obtemTextoDigitadoECriptografa();
+    var adiciona = adicionaAoRetangulo(input, encriptado);
+    var reseta = resetaTexto();
 });
 
 //desencriptografar
@@ -44,15 +48,21 @@ function copiaInput() {
 function obtemTextoDigitadoECriptografa(){
   caixaTexto = document.getElementById("caixa-teste").value;
   if (!caixaTexto) {
-  	alert("Digite uma mensagem para ser criptografada.");  
+    alert("Digite uma mensagem para ser criptografada.");  
+  }
+  else if(!regex.test(caixaTexto)){
+    alert('Utilize somente letras minúsculas e sem acentos.');
+    caixaTexto.value = '';
+    caixaTexto.focus();
+    return;  
   }
   else{
-  	novaCaixa = caixaTexto.replaceAll("a", "ai");
-  	novaCaixa2 = novaCaixa.replaceAll("e", "enter");
-  	novaCaixa3 = novaCaixa2.replaceAll("o", "ober");
-  	novaCaixa4 = novaCaixa3.replaceAll("u", "ufat");
-  	novaCaixa4 = novaCaixa3.replaceAll("i", "imea");
-  	return novaCaixa4;
+     mensagem = caixaTexto.replaceAll('e', 'enter');
+     mensagem = mensagem.replaceAll('i', 'imes');
+     mensagem = mensagem.replaceAll('a', 'ai');
+     mensagem = mensagem.replaceAll('o', 'ober');
+     mensagem = mensagem.replaceAll('u', 'ufat');
+     return mensagem;
 }};
 
 
@@ -71,14 +81,14 @@ function resetaTexto(){
 function obtemTextoDigitadoEDesencriptografa(){
   caixaTexto = document.getElementById("caixa-teste").value;
   if (!caixaTexto) {
-  	alert("Digite uma mensagem para ser descriptografada.");  
+    alert("Digite uma mensagem para ser descriptografada.");  
   }
   else{
-  novaCaixa = caixaTexto.replaceAll("ai", "a");
-  novaCaixa2 = novaCaixa.replaceAll("enter", "e");
-  novaCaixa3 = novaCaixa2.replaceAll("ober", "o");
-  novaCaixa4 = novaCaixa3.replaceAll("ufat", "u");
-  novaCaixa4 = novaCaixa3.replaceAll("imea", "i");
-  return novaCaixa4;
+    mensagemSaida = caixaTexto.replaceAll('enter', 'e');
+    mensagemSaida = mensagemSaida.replaceAll('imes', 'i');
+    mensagemSaida = mensagemSaida.replaceAll('ai', 'a');
+    mensagemSaida = mensagemSaida.replaceAll('ober', 'o');
+    mensagemSaida = mensagemSaida.replaceAll('ufat', 'u');
+    return mensagemSaida;
 }};
 
