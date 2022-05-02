a179e9ad3f51cded648abb1b168afc8eff5e229c
Yuri Sampaio <sampaio.yuri31@gmail.com>
2022-05-02 20:19:05 -0300

    adicionando validadores

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,19 @@ var regex = /^[a-z\s]+$/;
 
 //faz a foto que está dentro do retangulo e o texto sair do display
 function myFunction() {
-	caixaTexto = document.getElementById("caixa-teste").value;
-	if (caixaTexto) {
-		document.getElementById("foto").style.display = "none";
+  caixaTexto = document.getElementById("caixa-teste").value;
+  if (regex.test(caixaTexto)) {
+    document.getElementById("foto").style.display = "none";
 }};
 
 //criptografar
 var encriptar = document.querySelector("#criptografar"); // obtem o botão do html
 encriptar.addEventListener("click", function(){ // escuta o click e aplica a função
-  	var encriptado = obtemTextoDigitadoECriptografa();
-  	var adiciona = adicionaAoRetangulo(input, encriptado);
-  	var reseta = resetaTexto();
+    var encriptado = obtemTextoDigitadoECriptografa();
+    var adiciona = adicionaAoRetangulo(input, encriptado);
+    var reseta = resetaTexto();
+    caixaTexto.focus();
+    
 });
 
 //desencriptografar
@@ -28,6 +30,7 @@ desencriptar.addEventListener("click", function(){
   var desencriptado = obtemTextoDigitadoEDesencriptografa();
   var adiciona = adicionaAoRetangulo(input, desencriptado);
   var reseta = resetaTexto();
+  caixaTexto.focus();
 });
 
 //copiar
@@ -36,9 +39,10 @@ copiar.addEventListener('click', function(){
   navigator.clipboard.writeText(input.value);
   var reseta = resetaTexto();
   alert('Mensagem copiada para a área de transferência.')
+  caixaTexto.focus();
 });
 
-
+//funções
 function copiaInput() {
   var copiaTexto = document.getElementById("input")
   adicionaValor.textContent = copiaTexto;
@@ -46,23 +50,22 @@ function copiaInput() {
 };
 
 function obtemTextoDigitadoECriptografa(){
-  caixaTexto = document.getElementById("caixa-teste").value;
-  if (!caixaTexto) {
-  	alert("Digite uma mensagem para ser criptografada.");  
+  caixaTexto = document.getElementById("caixa-teste");
+  if (!caixaTexto.value) {
+    alert("Digite uma mensagem para ser criptografada.");
+    caixaTexto.focus(); 
   }
-  else if(!regex.test(caixaTexto)){
+  else if(!regex.test(caixaTexto.value)){
     alert('Utilize somente letras minúsculas e sem acentos.');
-    caixaTexto.value = '';
     caixaTexto.focus();
-    return;  
   }
   else{
-  	 mensagem = caixaTexto.replaceAll('e', 'enter');
+     mensagem = caixaTexto.value.replaceAll('e', 'enter');
      mensagem = mensagem.replaceAll('i', 'imes');
      mensagem = mensagem.replaceAll('a', 'ai');
      mensagem = mensagem.replaceAll('o', 'ober');
      mensagem = mensagem.replaceAll('u', 'ufat');
-  	 return mensagem;
+     return mensagem;
 }};
 
 
@@ -79,16 +82,16 @@ function resetaTexto(){
 };
 
 function obtemTextoDigitadoEDesencriptografa(){
-  caixaTexto = document.getElementById("caixa-teste").value;
-  if (!caixaTexto) {
-  	alert("Digite uma mensagem para ser descriptografada.");  
+  caixaTexto = document.getElementById("caixa-teste");
+  if (!caixaTexto.value) {
+    alert("Digite uma mensagem para ser descriptografada.");
+    caixaTexto.focus();   
   }
   else{
-    mensagemSaida = caixaTexto.replaceAll('enter', 'e');
+    mensagemSaida = caixaTexto.value.replaceAll('enter', 'e');
     mensagemSaida = mensagemSaida.replaceAll('imes', 'i');
     mensagemSaida = mensagemSaida.replaceAll('ai', 'a');
     mensagemSaida = mensagemSaida.replaceAll('ober', 'o');
     mensagemSaida = mensagemSaida.replaceAll('ufat', 'u');
     return mensagemSaida;
 }};
-
